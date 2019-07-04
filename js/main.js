83129d9f02b591147c01fcdc325d83b687995534
Alice Lima <alice_lima2@live.com>
2019-07-04 08:06:53 -0300

    Medicacao

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
 function atualizaData(){
-		// Obtém a data/hora atual
-		var data = new Date();
+	// Obtém a data/hora atual
+	var data = new Date();
 
-		// Guarda cada pedaço em uma variável
-		var dia     = data.getDate();           // 1-31
-		var mes     = data.getMonth();          // 0-11 (zero=janeiro)
-		var ano     = data.getFullYear();       // 4 digitos
-		var dia_sem = data.getDay();            // 0-6 (zero=domingo)
-		var dias = new Array(
- 'Domingo','Segunda-feira','Terça-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sábado'
+	// Guarda cada pedaço em uma variável
+	var dia     = data.getDate();           // 1-31
+	var mes     = data.getMonth() + 1;          // 0-11 (zero=janeiro)
+	var ano     = data.getFullYear();       // 4 digitos
+	var dia_sem = data.getDay();            // 0-6 (zero=domingo)
+	var dias = new Array(
+'Domingo','Segunda-feira','Terça-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sábado'
 );
 
-	// Formata a data e a hora (note o mês + 1)
-	str_data = dias[dia_sem] + ', ' + dia + '/0' + (mes+1) + ano;
+// Formata a data e a hora (note o mês + 1)
 
-	document.getElementById("lblData").innerHTML = str_data;
+if(dia < 10) {
+	dia = '0' + dia;
+}
+
+if((mes) < 10) {
+	mes = '0' + mes;
+}
+
+str_data = dias[dia_sem] + ', ' + dia + '/' + (mes) + '/' + ano;
+
+document.getElementById("lblData").innerHTML = str_data;
+}
+
+jQuery(window).load(function($){
+	atualizaData();
+});
+
+function hide(element){
+	element.className = "invisivel";
+}
+
+function show(element){
+	element.className = "visivel";
+}
+
+function changeClass(element) {
+	if(element.className != 'mudarCor' ){
+		window.location.href='Consulta.html';
+	}
+	else {
+		element.className = 'mudarCor';
+	}
 }
